609aa979c4b65533f8111fad5e1146190c2088c2
adr <none@none.com>
2023-06-30 18:28:43 +0300

    53

--- gitlab_pipeline_check/pipelines_report5.py
+++ gitlab_pipeline_check/pipelines_report5.py
@@ -1,60 +1,4 @@
-import gitlab
-import pandas as pd
-
-def process_pipelines(project_id):
-    pipeline_ids = get_pipeline_ids()
-
-    pipeline_statuses = []
-    failed_jobs = []
-
-    gl = gitlab.Gitlab(gitlab_url, private_token=access_token)
-    project = gl.projects.get(project_id)
-
-    for pipeline_id in pipeline_ids:
-        pipeline = project.pipelines.get(pipeline_id)
-        pipeline_name = pipeline.name
-        pipeline_status = pipeline.status
-        job_ids = []
-        job_names = []
-        job_logs = []
-
-        for job in pipeline.jobs.list():
-            job_ids.append(job.id)
-            job_names.append(job.name)
-            job_logs.append(get_failed_job_logs(job))
-
-        if pipeline_status == 'success':
-            pipeline_statuses.append({'Pipeline ID': pipeline_id, 'Status': 'OK'})
-            failed_jobs.append({
-                'Pipeline ID': pipeline_id,
-                'Pipeline Name': pipeline_name,
-                'Jobs Names': 'N/A',
-                'Jobs IDs': 'N/A',
-                'Job Logs': 'N/A'
-            })
-        elif pipeline_status == 'failed':
-            failed_jobs.append({
-                'Pipeline ID': pipeline_id,
-                'Pipeline Name': pipeline_name,
-                'Jobs Names': job_names,
-                'Jobs IDs': job_ids,
-                'Job Logs': job_logs
-            })
-
-    generate_report(pipeline_statuses, failed_jobs)
-
-def generate_report(pipeline_statuses, failed_jobs):
-    report_data = []
-
-    for pipeline_id, status in pipeline_statuses.items():
-        report_data.append({'Pipeline ID': pipeline_id, 'Status': status})
-
-    for job_id, job_data in failed_jobs.items():
-        report_data.append({'Failed Job ID': job_id, 'Job Name': job_data['name'], 'Job Logs': job_data['logs']})
-
-    for pipeline_id in successful_pipelines:
-        report_data.append({'Pipeline ID': pipeline_id, 'Status': 'success'})
-
+def generate_report(report_data):
     df = pd.DataFrame(report_data)
 
     def color_cells(val):
@@ -72,3 +16,41 @@ def generate_report(pipeline_statuses, failed_jobs):
         file.write(report_html)
 
     print(f'Pipeline report generated: {filename}')
+
+
+def process_pipelines(project_id):
+    pipeline_ids = get_pipeline_ids()
+
+    pipeline_statuses = []
+    failed_jobs = []
+    successful_pipelines = []
+
+    for pipeline_id in pipeline_ids:
+        pipeline_name, job_names, job_ids, job_logs = get_pipeline_details(pipeline_id)
+
+        if pipeline_name:
+            pipeline_statuses.append({'Pipeline ID': pipeline_id, 'Pipeline Name': pipeline_name})
+
+            if job_names and job_ids:
+                pipeline_statuses[-1]['Jobs'] = ', '.join(job_names)
+                pipeline_statuses[-1]['Job IDs'] = ', '.join(job_ids)
+
+            if job_logs:
+                failed_jobs.append({'Pipeline ID': pipeline_id, 'Failed Job Logs': job_logs})
+            else:
+                successful_pipelines.append(pipeline_id)
+
+    generate_report(pipeline_statuses + failed_jobs + successful_pipelines)
+
+
+# def main():
+#     parser = argparse.ArgumentParser(description='Process pipeline IDs')
+#     parser.add_argument('project_id', type=int, help='Project ID')
+#     args = parser.parse_args()
+#     project_id = args.project_id
+
+process_pipelines(project_id)
+
+
+# if __name__ == '__main__':
+#     main()
